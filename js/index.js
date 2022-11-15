1a73cf1b3901a5edbb753dee0a621a0adaaee89c
Richard <55552821+Em1tt@users.noreply.github.com>
2022-11-15 20:02:43 +0100

    .

--- js/index.js
+++ js/index.js
@@ -7,3 +7,18 @@ window.addEventListener("scroll", () => {
         nav.classList.remove("scrolled");
     }
 })
+
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.maxHeight){
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + "px";
+    }
+  });
+}
